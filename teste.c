9aa2d6ad61498a076962699102fd0d98b5666310
Ithalorc <ithalossc@gmail.com>
2014-12-05 09:54:23 -0200

    Leitura, ordenação e escrita: OK(sem troca de arquivos)

--- teste.c
+++ teste.c
@@ -15,16 +15,18 @@ typedef struct candidato Candidato;
 
 main(){
 
-//char vetArquivos[] = {"candidatos.000","candidatos.001","candidatos.002","candidatos.003","candidatos.004","candidatos.005","candidatos.006","candidatos.007","candidatos.008","candidatos.009","candidatos.010","candidatos.011","candidatos.012","candidatos.013","candidatos.014","candidatos.015","candidatos.016","candidatos.017","candidatos.018","candidatos.019","candidatos.020","candidatos.021","candidatos.022","candidatos.023","candidatos.024","candidatos.025","candidatos.026","candidatos.027","candidatos.028","candidatos.029","candidatos.030","candidatos.031","candidatos.032","candidatos.033","candidatos.034","candidatos.035","candidatos.036","candidatos.037","candidatos.038","candidatos.039","candidatos.040","candidatos.041","candidatos.042","candidatos.043","candidatos.044","candidatos.045","candidatos.046","candidatos.047","candidatos.048","candidatos.049","candidatos.050","candidatos.051","candidatos.052","candidatos.053","candidatos.054","candidatos.055","candidatos.056","candidatos.057","candidatos.058","candidatos.059","candidatos.060","candidatos.061","candidatos.062","candidatos.063","candidatos.064","candidatos.065","candidatos.066","candidatos.067","candidatos.068};
+//char vetArquivos[] = {"candidatos.000","candidatos.001","candidatos.002","candidatos.003","candidatos.004","candidatos.005","candidatos.006","candidatos.007","candidatos.008","candidatos.009","candidatos.010","candidatos.011","candidatos.012","candidatos.013","candidatos.014","candidatos.015","candidatos.016","candidatos.017","candidatos.018","candidatos.019","candidatos.020","candidatos.021","candidatos.022","candidatos.023","candidatos.024","candidatos.025","candidatos.026","candidatos.027","candidatos.028","candidatos.029","candidatos.030","candidatos.031","candidatos.032","candidatos.033","candidatos.034","candidatos.035","candidatos.036","candidatos.037","candidatos.038","candidatos.039","candidatos.040","candidatos.041","candidatos.042","candidatos.043","candidatos.044","candidatos.045","candidatos.046","candidatos.047","candidatos.048","candidatos.049","candidatos.050","candidatos.051","candidatos.052","candidatos.053","candidatos.054","candidatos.055","candidatos.056","candidatos.057","candidatos.058","candidatos.059","candidatos.060","candidatos.061","candidatos.062","candidatos.063","candidatos.064","candidatos.065","candidatos.066","candidatos.067","candidatos.068};    // Vetor completo, será usado futuramente (teoricamente)
 
 FILE *fp;
-int x = 0, cont = 0, i = 0, m = 1, co = 0, l, k, verif = 1;
-char vetArquivos[2][15] = {"candidatos.000","candidatos.001"};
+int x = 0, cont = 0, i = 0, m = 1, co = 0, l, k, verif = 1;		//Variáveis de auxílio, a maioria
 
+// Vetor com o nome dos arquivos a serem lidos
+char vetArquivos[6][15] = {"candidatos.000","candidatos.001","candidatos.002","candidatos.003","candidatos.004","candidatos.005"}; 
 
-Candidato c[100], aux, v[100];
+// Vetor c[] que irá receber os arquivos e v[] que receberá, de c[], os registros de c[0](raíz) até ficar completo(ordenado)
+Candidato c[300], v[300], aux;
 
-	/* Lendo 6 arquivos */
+	/* Lendo registro dos 6 arquivos para o vetor */
 	while(verif != 0){
 		fp = fopen(vetArquivos[x],"rb");
 		if(fp == NULL){
@@ -33,33 +35,32 @@ Candidato c[100], aux, v[100];
 			break;
 		}else{
 		printf("Aberto arquivo %s\n\n",vetArquivos[x]);
+
 		fread(&c[i],sizeof(Candidato),i+1,fp);
 
+		/* O loop abaixo descobre o tamanho de cada arquivo e armazena o próximo espaço livre para início do próximo arquivo */
 		while(c[i].periodo != 0){
-			printf("i = %d, i+1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",i,i+1,
-				c[i].inscr,c[i].nome,c[i].periodo,c[i].turno,c[i].posicao,c[i].curso);
+		printf("i = %d, i+1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",i,i+1,   // [Impressão]
+				c[i].inscr,c[i].nome,c[i].periodo,c[i].turno,c[i].posicao,c[i].curso);											  // [Impressão]
 			i++;
 			fread(&c[i],sizeof(Candidato),SEEK_CUR,fp);
 		}
-		printf("Saiu do while\n\n");
 		fclose(fp);
 		x++;
 		}
 	}
 	/* Fim de Leitura */
 
-	/* escrevendo no arquivo de saída */
-	
-		// Heapsort
-	printf("Inicio HeapSort\n\n");
+	/* Ordenando vetor com registros dos 6 arquivos abertos */
+	printf("Inicio Heapsort\n\n");
 	verif = 1;
 	while(m <= i){
-	verif = 1;
+		verif = 1;
 		while(verif != 0){ 	// Se 'verif' retornar sem alterações, vetor já estará ordenado
 			verif = 0;			// Zera o atributo a cada nova varredura
-			for(k = 0; 2*k+2 <= i-m+1; k++){	
+			for(k = 0; 2*k+2 <= i-m; k++){	
 				l = 2*k+1;
-				if(strcmp(c[l].nome,c[l+1].nome) > 0)	// Verifica qual filho é o menor
+				if(strcmp(c[l].nome,c[l+1].nome) > 0)	// Verifica qual 'filho' é o menor
 					l++;
 				if(strcmp(c[l].nome,c[k].nome) < 0){ 	// Compara 'Pai' com 'menor filho'
 					aux = c[l];
@@ -70,70 +71,19 @@ Candidato c[100], aux, v[100];
 				}
 			}
 		}
-	printf("Contador de Loop: %d\n",co++);
-	for(k = 0; k < i-m+1 ; k++){
-		printf("k = %d Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,
-						c[k].inscr,c[k].nome,c[k].periodo,c[k].turno,c[k].posicao,c[k].curso);
-	}
-
-		printf("c[m-1] = %d %s e c[i-m] = %d %s\n",m-1,c[m-1].nome,i-m,c[i-m].nome);
-		v[m-1] = c[0];
-		c[0] = c[i-m];
-		m++;
+		v[m-1] = c[0];			// Próximo índice vazio de v[] recebe raíz de c[]
+		c[0] = c[i-m];			// Raíz de c[] recebe último índice do vetor
+		m++;						// Incrementa m para atualizar próximo índice vazio de v[] (v[m-1]) e reduzir de i no loop (i-m+1)
 	}	
-printf("\nPrint de V\n");
+	/* Fim de ordenação */	
 
-	for(k = 0; k < i ; k++){
-		printf("k = %d, k + 1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,k+1,
-						v[k].inscr,v[k].nome,v[k].periodo,v[k].turno,v[k].posicao,v[k].curso);
-	}
-		fp = fopen("candidatos.txt","wb");
-		fwrite(v,sizeof(Candidato),57,fp);
-		fclose(fp);
-
-	for(k = 0; k < i-m ; k++){
-		printf("k = %d, m = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,m,
-						c[k].inscr,c[k].nome,c[k].periodo,c[k].turno,c[k].posicao,c[k].curso);
-	}
-
-
-
-//	printf("\ncont = %d\n",cont);
-//	printf("\nPrint de C\n");
-//
-//	for(k = 0; k < i ; k++){
-//		printf("k = %d, k + 1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,k+1,
-//						c[k].inscr,c[k].nome,c[k].periodo,c[k].turno,c[k].posicao,c[k].curso);
-//	}
-//	
-//	printf("\nPrint de V\n");
-//
-//	for(k = 0; k < i ; k++){
-//		printf("k = %d, k + 1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,k+1,
-//						v[k].inscr,v[k].nome,v[k].periodo,v[k].turno,v[k].posicao,v[k].curso);
-//	}
-
-//	fp = fopen("candidatos.txt","wb");
-
-	//for(k = 0; k < i; k++)
-	//	fwrite(&c[0],sizeof(Candidato),1,fp);
-//	fclose(fp);
-	/* fim da escrita */
-
-	
-	/* Lendo arquivo novo */
-//	fp = fopen("candidatos.txt","wb");
-//	
-//	if(fp == NULL)
-//		printf("Erro ao abrir arquivo.\n");
-//	else{
-//		for(k = 0; k < i ; k++){
-//			fread(&c[k],sizeof(Candidato),k+1,fp);
-//			printf("k = %d, k + 1 = %d, Inscrição: %s, Nome: %s, Período: %d, Turno: %s, Posição: %d, Curso: %s\n",k,k+1,
-//						c[k].inscr,c[k].nome,c[k].periodo,c[k].turno,c[k].posicao,c[k].curso);
-//		}
+	/* Escreve Vetor ordenado no arquivo de saída */
+	fp = fopen("candidatos.txt","wb");
+	fwrite(v,sizeof(Candidato),i,fp);
+	fclose(fp);
 	/* Fim de leitura */
 
+	/* Leitura do novo arquivo, já ordenado - [Impressão] */
 	printf("Abrindo arquivo após Ordenação.\n\n");
 
 	fp = fopen("candidatos.txt","rb");
@@ -147,9 +97,8 @@ printf("\nPrint de V\n");
 						c[k].inscr,c[k].nome,c[k].periodo,c[k].turno,c[k].posicao,c[k].curso);
 		}
 	}
-	fclose(fp);	
-	// Heapsort
-	/* Ordenando vetor com 6 arquivos em memória - Heapsort */
+	fclose(fp);
+	/* Fim de leitura */	
 	
 }
 
